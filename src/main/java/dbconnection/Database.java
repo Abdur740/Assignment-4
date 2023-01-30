23f47d830439e43227aa53d32b1c887933303520
Abdur740 <123439024+Abdur740@users.noreply.github.com>
2023-01-30 15:46:19 +0530

    Update Database.java

--- src/main/java/dbconnection/Database.java
+++ src/main/java/dbconnection/Database.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 
 class Dbconnection{
-    static int status = 0;
+    int status = 0;
     
     public static final Logger LOGGER = Logger.getLogger("InfoLogging");
     static Dbconnection connection=null;    
@@ -23,16 +23,16 @@ class Dbconnection{
         }
         return connection;
     }
-    public static void newconnection(){
+    public void newconnection(){
         status = 1;
         LOGGER.info("connected at : {}",connection);
     }
-    public static void closeconnection(){
-        connection = null;
+    public void closeconnection(){
+        Dbconnection.connection = null;
         status=0;
         LOGGER.info("connection closed\nSelect option for new connection." );   
     }
-    public static void checkconnection(){
+    public void checkconnection(){
         if(status == 0){
             LOGGER.info("No connection");
         }
